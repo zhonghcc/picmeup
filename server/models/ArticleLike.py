5b09fe783d31e63d099deba0cc9a08c1348b312e
zhonghcc <zhonghcc@gmail.com>
2016-08-09 09:22:46 +0800

    add article like

---
+++ server/models/ArticleLike.py
@@ -0,0 +1,15 @@
+#-*-coding:utf-8-*-
+
+from server import db
+from datetime import datetime
+
+class ArticleLike(db.Model):
+    __tablename__ = 'article_likes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    article_id=db.Column(db.Integer,nullable=False)
+    author_id=db.Column(db.Integer,nullable=False)
+
+    created_time = db.Column(db.DateTime, default=datetime.now)
+    updated_time = db.Column(db.DateTime, default=datetime.now)
+    enabled = db.Column(db.Boolean, nullable=False,default=True)
