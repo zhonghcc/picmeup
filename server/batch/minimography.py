fe8d0cfbf9702cabd49363a5516fa2afa40b4479
zhonghcc <zhonghcc@gmail.com>
2016-08-13 15:32:07 +0800

    improve minimography spider

--- server/batch/minimography.py
+++ server/batch/minimography.py
@@ -1,40 +1,45 @@
-
 import basespider
 from models import Article
 import re
 
-class Minimography(basespider.BaseSpider):
 
+class Minimography(basespider.BaseSpider):
     def __init__(self):
         self.currentidx = 0
+        indexHtml = self.getHtml()
+        reg = r'http://minimography.com/\d{3}/'
+        imgre = re.compile(reg)
+        self.imglist = re.findall(imgre, indexHtml)
 
     def getSource(self):
         return "minimography"
 
     def getNext(self):
-        self.currentidx=self.currentidx+1
-        self.picName='%03.d'%self.currentidx
-        return 'http://minimography.com/%s/' % self.picName
+        url = self.imglist.pop(0)
+        self.picName = url[24:27]
+        return url
 
-    def processSingle(self,url,html):
+        # self.currentidx=self.currentidx+1
+        # self.picName='%03.d'%self.currentidx
+        # return 'http://minimography.com/%s/' % self.picName
+
+    def processSingle(self, url, html):
         try:
             self.article = Article()
-            self.article.origin=self.getSource()
-            self.article.origin_url=url
-            self.origName='minimography_%03.d_orig.jpg'%self.currentidx
-            self.article.file_name=self.origName #minimography_001_orig.jpg
-            self.article.title=self.getSource()+' '+self.picName
-            reg=r'http://.*download.*\"'
+            self.article.origin = self.getSource()
+            self.article.origin_url = url
+            self.origName = 'minimography_%s_orig.jpg' % self.picName
+            self.article.file_name = self.origName  # minimography_001_orig.jpg
+            self.article.title = self.getSource() + ' ' + self.picName
+            reg = r'http://.*download.*\"'
             imgre = re.compile(reg)
             imglist = re.findall(imgre, html)
             pic_url = imglist[0][0:-1]
 
-            self.article.pic_url=pic_url
-            return self.saveImage(pic_url,self.origName)
-        except Exception,e:
+            self.article.pic_url = pic_url
+            return self.saveImage(pic_url, self.origName)
+        except Exception, e:
             self.error(e)
             return False
 
         return True
-
-
