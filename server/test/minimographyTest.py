71b6e205aa49d2822d82261287b403e1aa9502ed
zhonghcc <zhonghcc@gmail.com>
2016-08-30 18:22:45 +0800

    add minimography test

---
+++ server/test/minimographyTest.py
@@ -0,0 +1,25 @@
+#-*-coding:utf-8-*-
+
+import unittest
+from batch.minimography import Minimography
+import app
+
+class MinimographyTest(unittest.TestCase):
+
+    def setUp(self):
+        #app.init()
+        self.spider = Minimography()
+        self.spider.init()
+        self.spider.postInit()
+        pass
+
+    def testProcessOne(self):
+        url = self.spider.getNext()
+        html = self.spider.getHtml(url)
+        result = self.spider.processSingle(url,html)
+        print html
+        print result
+        self.assertNotEqual(result,False)
+
+if __name__ =='__main__':
+    unittest.main()
