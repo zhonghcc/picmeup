d7ffbb49775c9536ab4fe788e6aab07caa52b34f
zhonghcc <zhonghcc@gmail.com>
2016-09-07 16:18:02 +0800

    improve masonry display

--- server/static/js/home.js
+++ server/static/js/home.js
@@ -3,6 +3,11 @@
  */
 $(function(){
     var $container = $('.grid');
+    $container.masonry({
+        itemSelector: '.grid-item',
+        gutter: 20,
+        isAnimated: true,
+    });
     $container.imagesLoaded(function() {
         $container.masonry({
             itemSelector: '.grid-item',
@@ -30,18 +35,18 @@ $(function(){
         function( newElements ) {
             // hide new items while they are loading
             var $newElems = $( newElements ).css({ opacity: 0 });
-            // ensure that images load before adding to masonry layout
+            $container.masonry('appended', $newElems, true);
             $newElems.imagesLoaded(function(){
                 // show elems now they're ready
                 $newElems.animate({ opacity: 1 });
-                $container.masonry('appended', $newElems, true);
+                $container.masonry();
                 readedpage++;//当前页滚动完后，定位到下一页
                 if(readedpage>totalpage){//如果滚动到超过最后一页，置成不要再滚动。
                     $("#page-nav").remove();
                     $container.infinitescroll({state:{isDone:true}});
                 }else{
                     //'#page-nav a置成下一页的值
-                    $("#page-nav a").attr("href","/billstudy/bill/question/all/null/"+readedpage);
+                    $("#page-nav a").attr("href","/page/"+readedpage);
                 }
             });
         }
